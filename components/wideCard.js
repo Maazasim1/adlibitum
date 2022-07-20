c0379ecc2f5793b70b52a7d88e8de2fab7c540e6
Maazasim1 <58209878+Maazasim1@users.noreply.github.com>
2022-07-21 00:01:12 +0500

    fixed typos

--- components/wideCard.js
+++ components/wideCard.js
@@ -46,7 +46,7 @@ export default function WideCard() {
                 </div>
             </div>
             <div className='right-0 rounded-lg w-full sm:h-full h-[60vw] sm:w-[70%]'>
-                <iframe className='rounded-lg' width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=farhan%20classic+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure acres/hectares on map</a></iframe>
+                <iframe className='rounded-lg' width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=farhan%20classic+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure acres/hectares on map</a></iframe>
             </div>
 
         </div>
