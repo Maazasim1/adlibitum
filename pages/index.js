13dd6c4692e61b53974c8110cc1b33af3ebb182f
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-08-10 23:00:48 +0500

    added content

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,15 @@ export default function Home() {
               WHAT ARE WE?
             </h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum convallis at vel elit. Quisque quis tincidunt elit. Quisque imperdiet ligula tortor, sagittis maximus enim efficitur vitae. Vivamus dignissim nulla nec lectus tincidunt scelerisque. Sed auctor porttitor ligula nec egestas. Aenean eget volutpat diam, sit amet egestas tellus. Mauris laoreet mollis orci vitae vulputate. Ut maximus nulla sed commodo rhoncus. Aenean ac placerat magna.
+              Adlibitum is basically a trading company, operating in corporate sector supplying consumer
+              Electronics, mobile communications, home and kitchen appliances, office equipment, LED
+              lights, and power supply products.
+              We offer extended warranties and special offers that are provided by our suppliers and
+              product manufacturer. We also offer delivery services all over Pakistan for our corporate
+              customers.
+              Adlibitum provides extensive sales distribution and marketing services for electronic products
+              across the country. We exceed our customer expectation in quality, delivery and cost through
+              continuous improvement and customer interaction.
             </p>
             <Link href='#'>
               <a className='no-underline px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>
@@ -40,45 +48,52 @@ export default function Home() {
       </div>
       <div id="business" className='flex justify-center p-10'>
         <article className='prose text-center'>
-          <h1>OUR BUSINESS</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum convallis at vel elit. Quisque quis tincidunt elit. Quisque imperdiet ligula tortor, sagittis maximus enim efficitur vitae. Vivamus dignissim nulla nec lectus tincidunt scelerisque. Sed auctor porttitor ligula nec egestas. Aenean eget volutpat diam, sit amet egestas tellus. Mauris laoreet mollis orci vitae vulputate. Ut maximus nulla sed commodo rhoncus. Aenean ac placerat magna.
+          <h1>OUR VISION</h1>
+          <p>is to be the top marketing and distribution company to create a better everyday
+            life for many people by providing the highest level of service, the broadest selection of
+            products and the most competitive prices.
           </p>
         </article>
 
       </div>
-      <div  className="sm:p-20 flex sm:flex-row justify-center flex-col">
-        <Card src='images/delivery.svg' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' title='Door To Door Deliveries' />
-        <Card src='images/partner.svg' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' title='Affiliate Partners' />
-        <Card src='images/reliable.svg' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' title='Reliable & Dependable' />
+      <div className="sm:p-20 flex sm:flex-row justify-center flex-col">
+        <Card src='images/delivery.svg' text='Door to Door deliveries are made for bank credit card holders and corporate customers.' title='Door To Door Deliveries' />
+        <Card src='images/partner.svg' text='We have hundreds of prominent affiliate partners in all major cities of Pakistan.' title='Affiliate Partners' />
+        <Card src='images/reliable.svg' text='TVONICS Pakistan is a reliable and dependable source of supplier for all electronics appliances under one roof.' title='Reliable & Dependable' />
       </div>
       <div className='flex flex-col sm:flex-row justify-center bg-sky-300 p-20'>
         <article className='prose text-left'>
           <h1>
-            WHAT WE DO?
+            OUR MISSION
           </h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum convallis at vel elit. Quisque quis tincidunt elit. Quisque imperdiet ligula tortor, sagittis maximus enim efficitur vitae. Vivamus dignissim nulla nec lectus tincidunt scelerisque. Sed auctor porttitor ligula nec egestas. Aenean eget volutpat diam, sit amet egestas tellus. Mauris laoreet mollis orci vitae vulputate. Ut maximus nulla sed commodo rhoncus. Aenean ac placerat magna.</p>
+          <p>is to set new standards of excellence our mission is to build on our history of
+            product excellence and technology innovation to highest quality and most reliable products
+            and services to our nationwide customers.
+          </p>
+          <h1>
+            OUR STRENGTH
+          </h1>
+          <p>
+            has been the heartwarming testimonials of loyal customers that has taken us
+            from strength to strength. Excellent customer contacts, broad range of electrical and
+            electronic products, good pricing strategy and the team of experienced qualified and young
+            competent members are our back bone.
 
-
+          </p>
         </article>
         <img src='images/team.svg' className='h-21 w-21 pt-20 sm:pt-0' />
 
       </div>
       <div id="clients" className='sm:p-20'>
-        <div className='flex justify-center'>
-
-          <article className='prose p-20 text-center w-full align-center'>
-            <h1>
-              OUR CLIENTS
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum convallis at vel elit. Quisque quis tincidunt elit. Quisque imperdiet ligula tortor, sagittis maximus enim efficitur vitae. Vivamus dignissim nulla nec lectus tincidunt scelerisque. Sed auctor porttitor ligula nec egestas. Aenean eget volutpat diam, sit amet egestas tellus. Mauris laoreet mollis orci vitae vulputate. Ut maximus nulla sed commodo rhoncus. Aenean ac placerat magna.
-            </p>
-          </article>
-        </div>
         <div className='sm:p-20 flex flex-col sm:flex-row justify-center'>
-          <Card src="images/bank.svg" title="BANKS" text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' />
-          <Card src="images/medicine.svg" title="HOSPITALS" text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' />
-          <Card src="images/pharma.svg" title="PHARMACEUTICALS" text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae neque et urna rutrum' />
+          <Card src="images/bank.svg" title="BANKS" text='SILK BANK
+          HABIB BANK LIMITED' />
+          <Card src="images/medicine.svg" title="COMPANIES" text='PHILIP ELECTRONICS
+          SIGNIEY
+          EURO BUILDERS
+          KARACHI WATER & SEWERAGEE' />
+          <Card src="images/pharma.svg" title="PHARMACEUTICALS" text='PHARM EVO
+          BOSCH PAKISTAN' />
         </div>
       </div>
       <div id="manage" className='flex flex-row justify-center p-20'>
